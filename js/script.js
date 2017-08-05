8ea3a8a7cd58b1643c8e7e804a4ff2cde64a472f
chris flan <flanthedev@gmail.com>
2017-08-05 23:02:20 +0800

    set variables and make select lists for HTML

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const sideInput = document.getElementById('sideInput');
+const dieInput = document.getElementById('dieInput');
+const button = document.querySelector('button');
+const resultDiv = document.querySelector('#resultDiv');
+let results = [];
+
+let selectDieInput = document.getElementById('dieInput');
+for (i = 1; i < 99; i++) {
+  let option = document.createElement('option');
+  option.innerHTML = i
+  selectDieInput.appendChild(option);
+}
+
+let selectSideInput = document.getElementById('sideInput');
+for (i = 1; i < 99; i++) {
+  let option = document.createElement('option');
+  option.innerHTML = i
+  selectSideInput.appendChild(option);
+}
