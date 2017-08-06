248e241c2a1ba1eb97751f97f7d4b06698d926e9
chris flan <flanthedev@gmail.com>
2017-08-06 12:34:07 +0800

    refactor js and remove console logs

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 const sideInput = document.getElementById('sideInput');
 const dieInput = document.getElementById('dieInput');
 const button = document.querySelector('button');
-const resultDiv = document.querySelector('#resultDiv');
+const resultDiv = document.getElementById('resultDiv');
 let results = [];
 
-let selectDieInput = document.getElementById('dieInput');
-for (i = 1; i < 99; i++) {
+const selectDieInput = document.getElementById('dieInput');
+for (i = 2; i < 99; i++) {
   let option = document.createElement('option');
-  option.innerHTML = i
+  option.innerText = i;
   selectDieInput.appendChild(option);
 }
 
-let selectSideInput = document.getElementById('sideInput');
-for (i = 1; i < 99; i++) {
+const selectSideInput = document.getElementById('sideInput');
+for (i = 6; i < 99; i++) {
   let option = document.createElement('option');
-  option.innerHTML = i
+  option.innerText = i;
   selectSideInput.appendChild(option);
 }
 
@@ -26,26 +26,22 @@ let dice = {
   }
 }
 
-button.addEventListener("click", function() {
-  console.log("test");
+button.addEventListener("click", () => {
   dice.sides = parseInt(sideInput.value);
   let dieNumber = parseInt(dieInput.value);
 
   for (i = 0; i < dieNumber; i++) {
     let result = dice.roll();
     results[i] = result;
-    console.log(results);
   }
 
   let resultHTML = '<ul>';
   let resultSum = 0;
   for (i = 0; i < results.length; i++) {
-    resultHTML += '<li>' + results[i] + '</li>';
-    resultHTML += ' ';
+    resultHTML += '<li>' + results[i] + '</li> ';
     resultSum += results[i];
   }
   resultHTML += '</ul>';
   resultHTML += '<p> for a total of: ' + resultSum + '</p>';
-
   resultDiv.innerHTML = resultHTML;
 });
